1c909daa1c52d3cf8ee31d2b60c3adfd3bc0ada4
Xin Gao <xingao@upenn.edu>
2013-06-26 20:03:56 +0000

    limit the output size of text search to 20000 records.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/OrthoMCLWebService/trunk@56653 735e2a04-f8fc-0310-8a1b-f2942603c481

--- WSFPlugin/src/java/org/orthomcl/wsfplugin/KeywordSearchPlugin.java
+++ WSFPlugin/src/java/org/orthomcl/wsfplugin/KeywordSearchPlugin.java
@@ -24,100 +24,109 @@ import org.gusdb.wsf.plugin.WsfServiceException;
  * @created Nov 2012
  */
 public class KeywordSearchPlugin extends AbstractOracleTextSearchPlugin {
-  
+
   private static final String CTX_CONTAINER_APP = "wdkModel";
-  
+
   private static final String CONNECTION_APP = WdkModel.CONNECTION_APP;
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.gusdb.wsf.WsfPlugin#execute(java.util.Map, java.lang.String[])
-     */
-    public WsfResponse execute(WsfRequest request) throws WsfServiceException {
-        logger.info("Invoking OrthomclKeywordSearchPlugin...");
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.gusdb.wsf.WsfPlugin#execute(java.util.Map, java.lang.String[])
+   */
+  public WsfResponse execute(WsfRequest request) throws WsfServiceException {
+    logger.info("Invoking OrthomclKeywordSearchPlugin...");
 
-        // get parameters
-        Map<String, String> params = request.getParams();
-	String recordType = params.get(PARAM_WDK_RECORD_TYPE).trim().replaceAll("'", "");
-        String fields = params.get(PARAM_DATASETS).trim().replaceAll("'", "");
-        logger.debug("fields = \"" + fields + "\"");
-        String textExpression = params.get(PARAM_TEXT_EXPRESSION).trim();
-	String detailTable = params.get(PARAM_DETAIL_TABLE).trim().replaceAll("'", "");
-	String primaryKeyColumn = params.get(PARAM_PRIMARY_KEY_COLUMN).trim().replaceAll("'", "");
-	String projectId = params.get(PARAM_PROJECT_ID).trim().replaceAll("'", "");
+    // get parameters
+    Map<String, String> params = request.getParams();
+    String recordType = params.get(PARAM_WDK_RECORD_TYPE).trim().replaceAll(
+        "'", "");
+    String fields = params.get(PARAM_DATASETS).trim().replaceAll("'", "");
+    logger.debug("fields = \"" + fields + "\"");
+    String textExpression = params.get(PARAM_TEXT_EXPRESSION).trim();
+    String detailTable = params.get(PARAM_DETAIL_TABLE).trim().replaceAll("'",
+        "");
+    String primaryKeyColumn = params.get(PARAM_PRIMARY_KEY_COLUMN).trim().replaceAll(
+        "'", "");
+    String projectId = params.get(PARAM_PROJECT_ID).trim().replaceAll("'", "");
 
-	// get fields as quoted list
-        StringBuffer quotedFields = new StringBuffer("");
-        boolean notFirst = false;
-        String[] ds = fields.split(",\\s*");
-        for (String field : ds) {
-            if (notFirst) quotedFields.append(",");
-            quotedFields.append("'" + field + "'");
-            notFirst = true;
-        }
-
-        String oracleTextExpression = transformQueryString(textExpression);
-	PreparedStatement ps = null;
-        Map<String, SearchResult> matches = new HashMap<String, SearchResult>();
-	try {
-	    ps = getQuery(recordType, detailTable, primaryKeyColumn, projectId,
-			  oracleTextExpression, quotedFields.toString());
-	    matches = textSearch(ps, primaryKeyColumn);
-	} catch (SQLException | WdkModelException | EuPathServiceException ex) {
-	    throw new WsfServiceException(ex);
-	} finally {
-	    SqlUtils.closeStatement(ps);
-	}
-
-        // construct results
-        String[][] result = flattenMatches(getMatchesSortedArray(matches), request.getOrderedColumns());
-        WsfResponse wsfResult = new WsfResponse();
-        wsfResult.setResult(result);
-        wsfResult.setSignal(0);
-        return wsfResult;
+    // get fields as quoted list
+    StringBuffer quotedFields = new StringBuffer("");
+    boolean notFirst = false;
+    String[] ds = fields.split(",\\s*");
+    for (String field : ds) {
+      if (notFirst)
+        quotedFields.append(",");
+      quotedFields.append("'" + field + "'");
+      notFirst = true;
     }
 
-
-    private PreparedStatement getQuery(String recordType, String detailTable,
-				       String primaryKeyColumn, String projectId,
-				       String oracleTextExpression, String fields)
-	throws WsfServiceException,
-            SQLException, WdkModelException, EuPathServiceException {
-        Connection dbConnection = getDbConnection(CTX_CONTAINER_APP, CONNECTION_APP);
-
-        String sql = new String(
-				"select " + primaryKeyColumn + ",\n"
-			        + "       '" + projectId + "' as project_id, max(scoring) as max_score,\n"
-			        + "       apidb.tab_to_string(set(CAST(COLLECT(field_name) as apidb.varchartab)), ', ')\n"
-			        + "        as fields_matched\n"
-			        + "from (select  " + primaryKeyColumn + ", field_name, score(1) as scoring\n"
-			        + "      from " + detailTable + "\n"
-			        + "      where field_name in (" + fields + ")\n"
-			        + "        and contains(content, ?, 1) > 0)\n"
-			        + "group by  " + primaryKeyColumn + "\n"
-			        + "order by max(scoring) desc");
-        logger.debug("SQL: " + sql);
-        logger.debug("oracleTextExpression = \"" + oracleTextExpression + "\"");
-        logger.debug("fields = \"" + fields + "\"");
-        logger.debug("recordType = \"" + recordType + "\"");
-
-        PreparedStatement ps = null;
-        try {
-            ps = dbConnection.prepareStatement(sql);
-            ps.setString(1, oracleTextExpression);
-        } catch (SQLException e) {
-            logger.info("caught SQLException " + e.getMessage());
-            throw new WsfServiceException(e);
-        }
-
-        return ps;
+    String oracleTextExpression = transformQueryString(textExpression);
+    PreparedStatement ps = null;
+    Map<String, SearchResult> matches = new HashMap<String, SearchResult>();
+    try {
+      ps = getQuery(recordType, detailTable, primaryKeyColumn, projectId,
+          oracleTextExpression, quotedFields.toString());
+      matches = textSearch(ps, primaryKeyColumn);
+    } catch (SQLException | WdkModelException | EuPathServiceException ex) {
+      throw new WsfServiceException(ex);
+    } finally {
+      SqlUtils.closeStatement(ps);
     }
 
+    // construct results
+    StringBuilder message = new StringBuilder();
 
-    @Override
-    protected String[] defineContextKeys() {
-      return new String[] { CTX_CONTAINER_APP };
+    String[][] result = flattenMatches(getMatchesSortedArray(matches, message),
+        request.getOrderedColumns());
+    WsfResponse wsfResult = new WsfResponse();
+    wsfResult.setResult(result);
+    wsfResult.setSignal(0);
+    if (message.length() > 0)
+      wsfResult.setMessage(message.toString());
+    return wsfResult;
+  }
+
+  private PreparedStatement getQuery(String recordType, String detailTable,
+      String primaryKeyColumn, String projectId, String oracleTextExpression,
+      String fields) throws WsfServiceException, SQLException,
+      WdkModelException, EuPathServiceException {
+    Connection dbConnection = getDbConnection(CTX_CONTAINER_APP, CONNECTION_APP);
+
+    String sql = new String(
+        "select "
+            + primaryKeyColumn
+            + ",\n"
+            + "       '"
+            + projectId
+            + "' as project_id, max(scoring) as max_score,\n"
+            + "       apidb.tab_to_string(set(CAST(COLLECT(field_name) as apidb.varchartab)), ', ')\n"
+            + "        as fields_matched\n" + "from (select  "
+            + primaryKeyColumn + ", field_name, score(1) as scoring\n"
+            + "      from " + detailTable + "\n"
+            + "      where field_name in (" + fields + ")\n"
+            + "        and contains(content, ?, 1) > 0)\n" + "group by  "
+            + primaryKeyColumn + "\n" + "order by max(scoring) desc");
+    logger.debug("SQL: " + sql);
+    logger.debug("oracleTextExpression = \"" + oracleTextExpression + "\"");
+    logger.debug("fields = \"" + fields + "\"");
+    logger.debug("recordType = \"" + recordType + "\"");
+
+    PreparedStatement ps = null;
+    try {
+      ps = dbConnection.prepareStatement(sql);
+      ps.setString(1, oracleTextExpression);
+    } catch (SQLException e) {
+      logger.info("caught SQLException " + e.getMessage());
+      throw new WsfServiceException(e);
     }
+
+    return ps;
+  }
+
+  @Override
+  protected String[] defineContextKeys() {
+    return new String[] { CTX_CONTAINER_APP };
+  }
 
 }
