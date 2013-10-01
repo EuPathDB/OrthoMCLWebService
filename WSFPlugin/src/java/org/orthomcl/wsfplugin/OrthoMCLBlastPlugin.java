8a1988fcbb098c986415bba5a56f8618345cdd0d
Xin Gao <xingao@upenn.edu>
2013-10-01 22:34:16 +0000

    make data_type optional in orthomcl's blast.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/OrthoMCLWebService/branches/stream@58483 735e2a04-f8fc-0310-8a1b-f2942603c481

--- WSFPlugin/src/java/org/orthomcl/wsfplugin/OrthoMCLBlastPlugin.java
+++ WSFPlugin/src/java/org/orthomcl/wsfplugin/OrthoMCLBlastPlugin.java
@@ -14,6 +14,19 @@ public class OrthoMCLBlastPlugin extends AbstractBlastPlugin {
   @Override
   public String[] getRequiredParameterNames() {
     String[] partParams = super.getRequiredParameterNames();
+    
+    // replace data_type with database, since data_type is not needed here.
+    boolean replaced = false;
+    for (int i = 0; i < partParams.length;i++) {
+      if (partParams[i].equals(AbstractBlastPlugin.PARAM_DATA_TYPE)) {
+        partParams[i] = PARAM_DATABASE;
+        replaced = true;
+        break;
+      }
+    }
+    if (replaced) return partParams;
+    
+    // data_type not found, append the database param to the list
     String[] params = new String[partParams.length + 1];
     System.arraycopy(partParams, 0, params, 0, partParams.length);
     params[params.length - 1] = PARAM_DATABASE;
