b6f17aaf0c16e11f119da3e2c55cdc2d5ea8e36f
Xin Gao <xingao@upenn.edu>
2014-01-09 19:34:50 +0000

    fix the issue that has "source_id" in the url in blast result. changed it to "full_id".
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/OrthoMCLWebService/trunk@59799 735e2a04-f8fc-0310-8a1b-f2942603c481

--- WSFPlugin/src/java/org/orthomcl/wsfplugin/OrthoMCLBlastResultFormatter.java
+++ WSFPlugin/src/java/org/orthomcl/wsfplugin/OrthoMCLBlastResultFormatter.java
@@ -1,7 +1,10 @@
 package org.orthomcl.wsfplugin;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.sql.SQLException;
 
+import org.eupathdb.websvccommon.wsfplugin.EuPathServiceException;
 import org.eupathdb.websvccommon.wsfplugin.blast.NcbiBlastResultFormatter;
 
 
@@ -10,6 +13,19 @@ public class OrthoMCLBlastResultFormatter extends NcbiBlastResultFormatter {
   @Override
   protected String getProject(String organism) throws SQLException {
     return "OrthoMCL";
+  }
+
+  @Override
+  protected String getIdUrl(String recordClass, String projectId,
+      String sourceId) throws EuPathServiceException {
+    try {
+    String url = "showRecord.do?name=" + recordClass + "&project_id="
+        + URLEncoder.encode(projectId, "UTF-8") + "&full_id="
+        + URLEncoder.encode(sourceId, "UTF-8");
+    return url;
+    } catch (UnsupportedEncodingException ex) {
+      throw new EuPathServiceException(ex);
+    }
   }
 
 }
