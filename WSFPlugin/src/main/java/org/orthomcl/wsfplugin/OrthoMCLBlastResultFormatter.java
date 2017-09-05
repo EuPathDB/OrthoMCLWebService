7132b497c6815af5d1f284f41313bfba12810933
Ryan Doherty <tech@conical.org>
2017-09-05 20:48:42 +0000

    clean up Java compiler warnings
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/OrthoMCLWebService/trunk@84412 735e2a04-f8fc-0310-8a1b-f2942603c481

--- WSFPlugin/src/main/java/org/orthomcl/wsfplugin/OrthoMCLBlastResultFormatter.java
+++ WSFPlugin/src/main/java/org/orthomcl/wsfplugin/OrthoMCLBlastResultFormatter.java
@@ -1,12 +1,11 @@
 package org.orthomcl.wsfplugin;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import static org.gusdb.fgputil.FormatUtil.urlEncodeUtf8;
+
 import java.sql.SQLException;
 
 import org.eupathdb.websvccommon.wsfplugin.EuPathServiceException;
 import org.eupathdb.websvccommon.wsfplugin.blast.NcbiBlastResultFormatter;
-
 
 public class OrthoMCLBlastResultFormatter extends NcbiBlastResultFormatter {
 
@@ -18,14 +17,8 @@ public class OrthoMCLBlastResultFormatter extends NcbiBlastResultFormatter {
   @Override
   protected String getIdUrl(String recordClass, String projectId,
       String sourceId, String defline) throws EuPathServiceException {
-    try {
-    String url = "showRecord.do?name=" + recordClass + "&project_id="
-        + URLEncoder.encode(projectId, "UTF-8") + "&full_id="
-        + URLEncoder.encode(sourceId, "UTF-8");
-    return url;
-    } catch (UnsupportedEncodingException ex) {
-      throw new EuPathServiceException(ex);
-    }
+    return "showRecord.do?name=" + recordClass + "&project_id="
+        + urlEncodeUtf8(projectId) + "&full_id="
+        + urlEncodeUtf8(sourceId);
   }
-
 }
